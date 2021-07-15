72680c1b4f0c4620abb3d72a913e33c307dad8f7
reazzyg <mudrsemen@ya.ru>
2021-07-15 16:32:00 +0300

    create: added booking block and map

--- js/main.js
+++ js/main.js
@@ -11,3 +11,22 @@ const swiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   },
 });
+// Функция ymaps.ready() будет вызвана, когда
+    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map(document.querySelector('.map'), {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [7.890761189391702,98.29469223370225],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 17,
+            controls: ['smallMapDefaultSet']
+            }, {
+        searchControlProvider: 'yandex#search'
+    });
+    }
