ae5d9a269f791499a3abeb29ecbd5536075aa738
reazzyg <mudrsemen@ya.ru>
2021-07-26 17:37:41 +0300

    create: added closing with esc for modal window

--- js/main.js
+++ js/main.js
@@ -85,4 +85,12 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
   }
+  $(document).on('keydown', function (e) {
+    if (e.keyCode == 27) {
+      var modalOverlay = $('.modal__overlay');
+      var modalDialog = $('.modal__dialog');
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+    }
+  });
 });
